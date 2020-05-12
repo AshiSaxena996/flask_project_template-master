c34beae909600ff891e754be481c70bc02ca1a5d
ashi saxena <brightstarashi@gmail.com>
2020-05-12 13:09:53 +0530

    updated html pages

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,6 @@ from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
     return render_template('index.html',title='home')
 
@@ -27,7 +26,121 @@ def login():
     return render_template('login.html', title='Sign In')
 
 
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+    if request.method=='POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        cpassword = request.form.get('cpassword')
+        password = request.form.get('password')
+        print(cpassword, password, cpassword==password)
+        if username and password and cpassword and email:
+            if cpassword != password:
+                flash('Password do not match','danger')
+                return redirect('/register')
+            else:
+                if User.query.filter_by(email=email).first() is not None:
+                    flash('Please use a different email address','danger')
+                    return redirect('/register')
+                elif User.query.filter_by(username=username).first() is not None:
+                    flash('Please use a different username','danger')
+                    return redirect('/register')
+                else:
+                    user = User(username=username, email=email)
+                    user.set_password(password)
+                    db.session.add(user)
+                    db.session.commit()
+                    flash('Congratulations, you are now a registered user!','success')
+                    return redirect(url_for('login'))
+        else:
+            flash('Fill all the fields','danger')
+            return redirect('/register')
 
+    return render_template('register.html', title='Sign Up page')
+
+@app.route('/forgot',methods=['GET', 'POST'])
+def forgot():
+    if request.method=='POST':
+        email = request.form.get('email')
+        if email:
+            pass
+    return render_template('forgot.html', title='Password reset page')
+    
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@login_required
+@app.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('profile.html', user=user, title=f'{user.username} profile')
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    if request.method=='POST':
+        current_user.username = request.form.get('username')
+        current_user.about_me = request.form.get('aboutme')
+        db.session.commit()
+        flash('Your changes have been saved.','success')
+        return redirect(url_for('edit_profile'))
+    return render_template('edit_profile.html', title='Edit Profile',user=user)
+
+@app.route('/cuisine')
+@login_required
+def cuisine():
+    return render_template('cuisine.html', title='about_1')
+
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html', title='about_1')
+
+
+@app.route('/price')
+@login_required
+def price():
+    return render_template('price.html', title='about_1')
+
+@app.route('/history')
+@login_required
+def history():
+    return render_template('history.html', title='about_1')
+
+@app.route('/top_rest')
+@login_required
+def top_rest():
+    return render_template('top_rest.html', title='about_1')
+
+@app.route('/demo_map')
+@login_required
+def demo_map():
+    return render_template('demo_map.html', title='about_1')
+
+@app.route('/feature_selection')
+@login_required
+def feature_selection():
+    return render_template('feature_selection.html', title='about_1')
+
+
+
+
+
+
+
+
+# TEMPORARY PAGES FOR DATA INSERTION
 @app.route('/addcuisines',methods=['GET','POST'])
 def addcuisines():
     if request.method =='POST':
@@ -90,91 +203,10 @@ def addrest():
 
 
     
-@app.route('/register',methods=['GET', 'POST'])
-def register():
-    if request.method=='POST':
-        email = request.form.get('email')
-        username = request.form.get('username')
-        cpassword = request.form.get('cpassword')
-        password = request.form.get('password')
-        print(cpassword, password, cpassword==password)
-        if username and password and cpassword and email:
-            if cpassword != password:
-                flash('Password do not match','danger')
-                return redirect('/register')
-            else:
-                if User.query.filter_by(email=email).first() is not None:
-                    flash('Please use a different email address','danger')
-                    return redirect('/register')
-                elif User.query.filter_by(username=username).first() is not None:
-                    flash('Please use a different username','danger')
-                    return redirect('/register')
-                else:
-                    user = User(username=username, email=email)
-                    user.set_password(password)
-                    db.session.add(user)
-                    db.session.commit()
-                    flash('Congratulations, you are now a registered user!','success')
-                    return redirect(url_for('login'))
-        else:
-            flash('Fill all the fields','danger')
-            return redirect('/register')
-
-    return render_template('register.html', title='Sign Up page')
-
-
-@app.route('/forgot',methods=['GET', 'POST'])
-def forgot():
-    if request.method=='POST':
-        email = request.form.get('email')
-        if email:
-            pass
-    return render_template('forgot.html', title='Password reset page')
-    
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@login_required
-@app.route('/user/<username>')
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return render_template('profile.html', user=user, title=f'{user.username} profile')
-
-
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
-
-@app.route('/edit_profile', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    if request.method=='POST':
-        current_user.username = request.form.get('username')
-        current_user.about_me = request.form.get('aboutme')
-        db.session.commit()
-        flash('Your changes have been saved.','success')
-        return redirect(url_for('edit_profile'))
-    return render_template('edit_profile.html', title='Edit Profile',user=user)
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='about_1')
-
-@app.route('/cusine')
-def cuisine():
-    return render_template('cusine.html', title='about_1')
 
 
 
-@app.route('/search')
-def search():
-    return render_template('search.html', title='about_1')
+
 
 
 @app.route('/input',methods=['GET','POST'])
